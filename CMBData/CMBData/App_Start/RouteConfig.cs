5326231caaa4167ea45451c339bb5647d8bd22cd
msuresu <suresu.manivannan@one.verizon.com>
2015-08-27 17:16:37 -0700

    as

--- CMBData/CMBData/App_Start/RouteConfig.cs
+++ CMBData/CMBData/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CMBData
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "DataPrdictive", action = "Index", id = UrlParameter.Optional }
             );
         }
     }

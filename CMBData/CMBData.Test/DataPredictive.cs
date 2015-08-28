9786ff92d429620a2a3d5917ca64de8cc53df8f7
msuresu <suresu.manivannan@one.verizon.com>
2015-08-28 02:58:32 -0700

    adas

---
+++ CMBData/CMBData.Test/DataPredictive.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CMBDataAnalytics.Models;
+namespace CMBData.Test
+{
+    [TestFixture]
+    public class DataPredictive
+    {
+        [Test]
+        public void Test()
+        {
+            DataPredictiveModel objPredictMdl = new DataPredictiveModel();
+            Assert.IsNotNull(objPredictMdl);
+     
+        }
+    }
+}
--- CMBData/CMBData.Test/Properties/AssemblyInfo.cs
+++ CMBData/CMBData.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CMBData.Tests")]
+[assembly: AssemblyTitle("CMBData.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CMBData.Tests")]
+[assembly: AssemblyProduct("CMBData.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2c8c62f8-7ebe-4a08-935d-d302ffb32632")]
+[assembly: Guid("c1f378b2-c39d-407c-a8c5-a01ec6a4c363")]
 
 // Version information for an assembly consists of the following four values:
 //

a59f60a4b6920d05df5368feb26c3be11090455c
msuresu <suresu.manivannan@one.verizon.com>
2015-08-27 17:18:46 -0700

    view

---
+++ CMBData/CMBData/Controllers/DataPrdictiveController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CMBData.Controllers
+{
+    public class DataPrdictiveController : Controller
+    {
+        // GET: DataPrdictive
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult getPredictData(string strTN, string strRepeatCount, string strReason)
+        {
+            return Json("sucess");
+        }
+    }
+}
